79324d984be95ce0d0672e670ce1eb4a8cf3d435
jasonhaines613 <jasonhaines16@gmail.com>
2024-11-24 13:17:14 -0600

    corrected

---
+++ js/gallery.js
@@ -0,0 +1,28 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+   // console.log( "ready!" );
+
+
+   $("#galleryIMED1316").nanogallery2({
+  
+    thumbnailHeight:  "auto",
+    thumbnailWidth:   "450",
+    thumbnailBorderVertical: "0",
+    thumbnailBorderHorizontal: "0",
+    thumbnailLabel: {
+        position: "overImageOnBottom",
+        display: false
+    },
+thumbnailHoverEffect2: "imageScaleIn80|imageSepiaOfff|LabelApper75",
+thumbnailGutterWidth: 20,
+thumbnailGutterHeight: 20,
+thumbnailAlignment: "center",
+thumbnailOpenimage: true
+
+  });
+  
+
+
+
+
+});
